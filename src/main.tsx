7b9a9ab24afe397d9b04a1aa2ad71cd4f33c6434
stevenzg <guoyinfeng@live.com>
2023-09-10 10:31:02 +0800

    feat: config applet-apis

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { initApolloClient } from 'applet-apis'
+
 import App from './App'
 import './index.css'
+
+const { VITE_APPLET_AUTH_URL, VITE_APP_API_HOST } = import.meta.env
+
+initApolloClient({
+  graphQLUri: `${VITE_APP_API_HOST}/graphql`,
+  appletAuthUrl: VITE_APPLET_AUTH_URL
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
